fe5f25bb3acbdffa6ed7fb5fdc951bd034302aee
foops <dongyunlong-007@163.com>
2025-11-04 14:12:38 +0800

    Refactor ProtobufUtils and improve logging
    Refactor ProtobufUtils class methods and logging.

--- ppp.h
+++ ppp.h
@@ -1,59 +1,54 @@
 
 #pragma once
 
-#include "google/protobuf/util/json_util.h"
-#include "fslib/fs/FileSystem.h"
+#include <filesystem>
 #include <tuple>
 #include <vector>
+
+#include "autil/Log.h"
 #include "autil/StringUtil.h"
-#include <filesystem>
+#include "fslib/fs/FileSystem.h"
+#include "google/protobuf/util/json_util.h"
 
 namespace fs = std::filesystem;
 namespace suez {
-  
+AUTIL_DECLARE_AND_SETUP_LOGGER(suez, ProtobufUtils);
 
 class ProtobufUtils {
-
-public:
-  static bool protobufMessageToString(const google::protobuf::Message &message,
-                                      std::string &outputStr) {
-    return true;
-  }
-
-  static void printDirectory(std::string dir) {
-    std::cout <<"list directoy, dir:"<<dir<<std::endl;
-    for (const auto &entry : fs::directory_iterator(dir)) {
-      // 判断是否为目录
-      if (entry.is_directory()) {
-        std::cout << "list directoy [DIR]  " << entry.path().filename()<< std::endl;
-        printDirectory(entry.path());
-      } else {
-        std::cout << "list directoy [FILE] " << entry.path().filename()<< std::endl;
-      }
+   public:
+    static bool protobufMessageToString(const google::protobuf::Message& message, std::string& outputStr) {
+        return true;
     }
-  }
 
-  static bool fileToProtobufMessage(const std::string &fileName, google::protobuf::Message &message
-    , const std::vector<std::pair<std::string,std::string>> & replacePair={}) {
-
-    // std::cout << "load protobuf message from file: " << fileName << std::endl;
-    std::string content;
-    auto ec = fslib::fs::FileSystem::readFile(fileName, content);
-    if (ec != fslib::EC_OK) {
-      return false;
+    static void printDirectory(std::string dir) {
+        AUTIL_LOG
+        for (const auto1& entry : fs::directory_iterator(dir)) {
+            // 判断是否为目录
+            if (entry.is_directory()) {
+                AUTIL_LOG(INFO, "list directoy [DIR] %s", entry.path().filename().c_str());
+                printDirectory(entry.path());
+            } else {
+                AUTIL_LOG(INFO, "list directoy [FILE] %s", entry.path().filename().c_str());
+            }
+        }
     }
-    // std::cout <<"file content:"<<content<<std::endl;
-    for(const auto & pair : replacePair){
-        // std::cout <<" fileName:"<<fileName <<",replace, key="<<pair.first<<", value="<<pair.second<<std::endl;
-        autil::StringUtil::replaceAll(content, pair.first, pair.second);
-      } 
 
-    google::protobuf::util::JsonParseOptions options;
-    options.ignore_unknown_fields = true;
-    auto status =
-        google::protobuf::util::JsonStringToMessage(content, &message, options);
-    return status.ok();
-  }
+    static bool fileToProtobufMessage(const std::string& fileName, google::protobuf::Message& message,
+                                      const std::vector<std::pair<std::string, std::string>>& replacePair = {}) {
+        std::string content;
+        auto ec = fslib::fs::FileSystem::readFile(fileName, content);
+        if (ec != fslib::EC_OK) {
+            return false;
+        }
+        for (const auto& pair : replacePair) {
+            autil::StringUtil::replaceAll(content, pair.first, pair.second);
+        }
+
+        google::protobuf::util::JsonParseOptions options;
+        options.ignore_unknown_fields = true;
+        auto status = google::protobuf::util::JsonStringToMessage(content, &message, options);
+        return status.ok();
+    }
 };
 
-} // namespace suez
+}  // namespace suez
